2f3b4e68d9ebc091458410d644f6cc198995e9a4
manosakpujiha <manos.akpujiha@gmail.com>
2022-09-08 21:06:56 -0700

    Task 8 fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
 * main - Entry point
 *
-* Return: Always 0 (Success)
+* Return: Always 1 (Success)
 */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str1, 59);
 	return (1);
 }
