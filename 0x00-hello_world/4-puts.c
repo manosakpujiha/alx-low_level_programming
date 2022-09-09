9962cb74754126d665f79bd6ba6288a3573cd15c
manosakpujiha <manos.akpujiha@gmail.com>
2022-09-08 20:39:44 -0700

    Task 6 fix 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 
-
-
 /**
-
-   * main - prints a sentence
-
-    *
-
-     * Return: Always 0 (Success)
-
-      */
+ * main - prints a sentence
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
