2579a261cc9125b0fa068a231f4dcb81845b10db
manosakpujiha <manos.akpujiha@gmail.com>
2022-09-08 20:11:19 -0700

    Task 6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+
+   * main - prints a sentence
+
+    *
+
+     * Return: Always 0 (Success)
+
+      */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
