2142b3f833a566cbb8cdc411b8e27cb1f2fb04a3
manosakpujiha <manos.akpujiha@gmail.com>
2022-09-09 19:57:22 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: must be 0 for success
+ */
+
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
