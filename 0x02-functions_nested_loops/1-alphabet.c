c0cdaf11c207fe26bb346b2f35c8fd5208a00fe8
manosakpujiha <manos.akpujiha@gmail.com>
2022-09-13 14:45:44 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet from a - z
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+
+{
+	char c = 'a';
+
+	while(c <= 'z')
+	{
+	_putchar(c);
+	c++;
+	}
+	_putchar('\n');
+}
