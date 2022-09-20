d5f06155f656427086348dffef0e13ed380f2ee4
manos <uwmano@yahoo.com>
2022-09-20 00:35:42 -0400

    task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ * @str: parameter
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != 0)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+
+		i++;
+	}
+	_putchar('\n');
+}
