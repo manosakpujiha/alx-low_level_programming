81271f830d8d346f97220ace6746e8d7fa1ae861
manos <uwmano@yahoo.com>
2022-09-22 19:06:07 -0400

    Task 3

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *_strncpy- concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@n: number of bytes
+ *@src: A pointer to a character that will also be changed
+ *Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
