73d09e8c87b8b1e324074c653eb6a7256cd7ccaa
manos <uwmano@yahoo.com>
2022-09-22 18:59:16 -0400

    'Task 1'

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *_strncat - concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@n: number of bytes
+ *@src: A pointer to a character that will also be changed
+ *Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
