a1391693c190eb92f06c928b2f8ec785b400926b
manosakpujiha <manos.akpujiha@gmail.com>
2022-10-05 12:08:42 -0700

    Task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argv: array of aurument passed
+ * @argc: number of aurument passed
+ * Return: must be 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+		printf("%s\n", argv[0]);
+		return (0);
+}
