e97fe070621ec089c5c4205cee9b42496f1b0600
manos <uwmano@yahoo.com>
2022-10-06 13:36:16 -0400

    UPDATE

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -8,4 +8,4 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
