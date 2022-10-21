39782a8bf15d0a8805dedefeea77f6a6bd8d154f
manosakpujiha <manos.akpujiha@gmail.com>
2022-10-20 18:45:35 -0700

    Task fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * print_list - prints list.
  * @h: param list
@@ -11,10 +10,9 @@ size_t print_list(const list_t *h)
 	size_t l_node;
 
 	l_node = 0;
-
 	while (h != NULL)
 	{
-		if (h->str == NULL) 
+		if (h->str == NULL)
 		{
 			printf("[%d] %s\n", 0, "(nil)");
 		}
