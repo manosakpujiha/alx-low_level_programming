65157419d8d62b0a57393685aa5da10ac253ae4e
manosakpujiha <manos.akpujiha@gmail.com>
2022-10-24 16:59:05 -0400

    fix

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,7 @@
-#ifndef DOGO
-#define DOGO
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
+#ifndef LISTSH
+#define LISTSH
+#include <stddef.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -17,7 +16,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -29,8 +27,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-listint_t *find_listint_loop(listint_t *head);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
+
+int _putchar(char c);
+
 #endif
