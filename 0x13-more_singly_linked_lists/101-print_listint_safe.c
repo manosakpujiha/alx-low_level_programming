adc49676485283f0463fe473c44be5ba8de65463
manosakpujiha <manos.akpujiha@gmail.com>
2022-10-24 16:56:57 -0400

    fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -39,6 +39,7 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t len = 0;
 	int loop;
+    
 	listint_t *loopnode;
 	loopnode = find_listint_loop_pl((listint_t *) head);
 	for (len = 0, loop = 1; (head != loopnode || loop) && head != NULL; len++)
