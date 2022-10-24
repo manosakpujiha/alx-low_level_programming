64da818b442c578ef82ef5d7e5ca6ac1ba1177c1
manosakpujiha <manos.akpujiha@gmail.com>
2022-10-24 14:35:09 -0400

    Task 14

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,10 +1,8 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdio.h>
+#ifndef DOGO
+#define DOGO
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdio.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -31,6 +29,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *find_listint_loop(listint_t *head);
 listint_t *reverse_listint(listint_t **head);
-
-#endif /* LISTS_H */
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+#endif
