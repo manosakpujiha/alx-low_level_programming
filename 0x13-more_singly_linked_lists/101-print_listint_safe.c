f66cad4a27691856ae538932bff8b9a2d3613527
manosakpujiha <manos.akpujiha@gmail.com>
2022-10-24 16:48:37 -0400

    Task 14

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,21 +6,21 @@
 */
 size_t print_listint_safe(const listint_t *head)
 {
-size_t a;
-listint_t *b = (listint_t *)head;
+    size_t a;
 
-a = 0;
-while (b && b > b->next)
-{
-printf("[%p] %d\n", (void *)b, b->n);
-b = b->next;
-a++;
-}
-if (b)
-{
-printf("[%p] %d\n", (void *)b, b->n);
-printf("-> [%p] %d\n", (void *)b->next, b->next->n);
-a++;
-}
-return (a);
+    listint_t *b = (listint_t *)head;
+    a = 0;
+    while (b && b > b->next)
+    {
+        printf("[%p] %d\n", (void *)b, b->n);
+        b = b->next;
+        a++;
+    }
+    if (b)
+    {
+        printf("[%p] %d\n", (void *)b, b->n);
+        printf("-> [%p] %d\n", (void *)b->next, b->next->n);
+        a++;
+    }
+    return (a);
 }
